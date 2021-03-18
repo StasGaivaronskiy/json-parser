624becd3a8b9641234e70659d6181e8047303ef4
StasGaivaronskiy <banmings98@gmail.com>
2021-03-18 23:57:16 +0600

    small refactoring, renamed some functions

--- main.py
+++ main.py
@@ -3,41 +3,42 @@ import requests
 import json
 from constants import URL_UPCOMING_EVENTS, URL_EVENTS_ODDS
 
-#main func
+#Main func
 def main():
     #Get upcoming events id 
-    events_id = get_upcoming_events()
+    events_ids = get_upcoming_events_ids()
 
     #Get odds upcoming events
-    oddsEvents = get_odds_upcoming_events(events_id)
-    print (oddsEvents) 
+    odds_events = get_odds_upcoming_events(events_ids)
+    print (odds_events) 
 
-def  get_upcoming_events():
+#Get upcoming events ids 
+def  get_upcoming_events_ids():
     response = requests.get(URL_UPCOMING_EVENTS)
     if response.status_code != 200:
         logging.error('unable to get response from url: ' + URL_UPCOMING_EVENTS)
         return
-    upcomingEvents = response.json()
-    resultsUpcomingEvents = upcomingEvents['results']
-    lengthOfList = len (resultsUpcomingEvents)
-    events_id = []
-    for i in range  (0,lengthOfList):
-        event_id = resultsUpcomingEvents[i]['id']
-        events_id.append(event_id)
-    return events_id
+    upcoming_events = response.json()
+    results_upcoming_events = upcoming_events['results']
+    length_of_list = len(results_upcoming_events)
+    events_ids = []
+    for item in range  (0,length_of_list):
+        event_id = results_upcoming_events[item]['id']
+        events_ids.append(event_id)
+    return events_ids
 
 #Get odds upcoming events
-def get_odds_upcoming_events(events_id):
-    oddsEvents = [] 
-    for event_id in events_id:
+def get_odds_upcoming_events(events_ids):
+    odds_events = [] 
+    for event_id in events_ids:
         URL_GET_EVENTS_ODDS = URL_EVENTS_ODDS  + str(event_id)
         response = requests.get(URL_GET_EVENTS_ODDS)
         if response.status_code != 200:
             logging.error('unable to get response from url: ' + URL_EVENTS_ODDS)
             return
-        jsonResponseOddsEvents = response.json()
-        oddsEvents.append(jsonResponseOddsEvents)
-    return oddsEvents
+        json_response_odds_events = response.json()
+        odds_events.append(json_response_odds_events['results']['odds'])
+    return odds_events
   
-#start program
+#Start program
 main()
